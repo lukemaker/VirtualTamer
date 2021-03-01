e1042dc58a70b6d40dff8b49534e2f6927130aca
Lucas Corrêa <63137745+lukemaker@users.noreply.github.com>
2021-03-01 14:20:18 -0300

    Update Opcoes.cs

--- DT/Opcoes.cs
+++ DT/Opcoes.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace DT
@@ -21,6 +14,16 @@ namespace DT
         {
             boxDesign.Text = "Clássico (Preto e vermelho)";
             boxEstilo.Text = "Clássica";
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
