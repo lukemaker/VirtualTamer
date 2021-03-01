57f8edd14b87c5999f1a7b54d19655676247bb32
Lucas Corrêa <63137745+lukemaker@users.noreply.github.com>
2021-02-28 21:37:59 -0300

    Update Remodel.cs

--- Game/Remodel.cs
+++ Game/Remodel.cs
@@ -154,5 +154,32 @@ namespace DT
                     break;
             }
         }
+        // Não funcional:
+        
+        private void btnPetTreinar_Click(object sender, EventArgs e)
+        {
+            petImagem.Location = new Point(141, 73);
+            petAndando.Enabled = false;
+            treinamento.Enabled = true;
+
+        }
+        int treinando;
+        private void treinamento_Tick(object sender, EventArgs e)
+        {
+            treinando++;
+            if (treinando == 40)
+            {
+                Pet.descerFome();
+                Pet.checarTudo();
+                Pet.mensagemTreinar();
+                Pet.ganharExp();
+                Pet.Força += 1;
+                Pet.Vida += 1;
+                System.Threading.Thread.Sleep(250);
+                treinando = 0;
+                petAndando.Enabled = true;
+                treinamento.Stop();
+            }
+        }
     }
 }
