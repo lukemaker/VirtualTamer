266afeee41bac8d3897418945b2f7c1679d9ec4c
Lucas Corrêa <63137745+lukemaker@users.noreply.github.com>
2021-03-01 23:33:46 -0300

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace DT
+{
+    class Database
+    {
+        public string _Perfil = "user0321";
+        public string _NomeDoProduto = "Virtual Tamer";
+        public string _NomeDoSalvamento = "user0321";
+        public string _ArquivoDeSalvamento = "Data 1 - ";
+        public float _VersaoDoProduto = 0.43f;
+        public string slot1local = @"C:\dtame\save1.dt";
+        public string slot2local = @"C:\dtame\save2.dt";
+        public string info1 = @"C:\dtame\info1.dt";
+        public string info2 = @"C:\dtame\info2.dt";
+    }
+}
