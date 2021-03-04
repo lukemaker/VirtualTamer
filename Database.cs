9d38123669b05669fa02798b074c982477a0ed18
Lucas Corrêa <63137745+lukinia@users.noreply.github.com>
2021-03-04 01:51:31 -0300

    Update Database.cs

--- Database.cs
+++ Database.cs
@@ -1,17 +1,19 @@
-using System;
-using System.Text;
+using System.Drawing;
 
 namespace DT
 {
     class Database
     {
         public string _Perfil = "user0321";
-        public string _NomeDoProduto = "Virtual Tamer";
+        public string _NomeDoProduto = "Virtual Tamer Demo";
         public string _NomeDoSalvamento = "user0321";
-        public float _VersaoDoProduto = 0.61f;
+        public float _VersaoDoProduto = 0.98f;
         public string slot1local = @"C:\dtame\save1.dt";
         public string slot2local = @"C:\dtame\save2.dt";
         public string info1 = @"C:\dtame\info1.dt";
         public string info2 = @"C:\dtame\info2.dt";
+        Image eggCasual = Properties.Resources.eggCasualAnimation;
+        Image eggDark = Properties.Resources.eggDarkAnimation;
+        Image petToko = Properties.Resources.babytoko;
     }
 }
